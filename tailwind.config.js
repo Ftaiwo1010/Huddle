3e9e029867af4a313eb3255e996f82ed7e1db646
Oluwafemi <ftaiwo1010@gmail.com.noreply.github.com>
2025-02-09 17:41:02 +0100

    fourth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,7 @@ module.exports = {
          'max-3': '37rem',
          'max-4': '29rem',
          'max-5': '15.5rem',
-         'max-6': '22rem'
+         'max-6': '23rem'
       },
       boxShadow: {
         'round-shadow': '0 0 6px 1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)'
